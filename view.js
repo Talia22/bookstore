6ce2afa23ed6c53b13fc058effe865e6da8da013
Talia Friedman <taliafriedman22@gmail.com>
2024-10-10 13:43:49 +0300

    some style chnages

--- view.js
+++ view.js
@@ -14,6 +14,14 @@ const getBook = (book) => {
 
 };
 
+const getStarRating = (rating) => {
+    let stars = '';
+    for (let i = 1; i <= 5; i++) {
+        stars += `<span class="star ${i <= rating ? 'filled' : ''}">&#9733;</span>`;
+    }
+    return stars;
+};
+
 const renderBookList = (books) => {
     const startIndex = currentPage * itemsPerPage; 
     const endIndex = startIndex + itemsPerPage;
@@ -29,136 +37,3 @@ const renderBookList = (books) => {
     document.getElementById("prevBtn").disabled = currentPage === 0;
     document.getElementById("nextBtn").disabled = endIndex >= books.length;
 };
-
-document.getElementById("prevBtn").addEventListener("click", () => {
-    if (currentPage > 0) {
-        currentPage--; 
-        renderBookList(books);
-    }
-});
-
-document.getElementById("nextBtn").addEventListener("click", () => {
-    if ((currentPage + 1) * itemsPerPage < books.length) {
-        currentPage++; 
-        renderBookList(books);
-    }
-});
-
-const paginationButtons = (length) => {
-    const totalPages = Math.ceil(length / itemsPerPage);
-    const paginationContainer = document.getElementById("paginationButtons");
-    paginationContainer.innerHTML = '';
-
-    for (let i = 0; i < totalPages; i++) {
-        const pageButton = document.createElement("button");
-        pageButton.textContent = i + 1;
-        pageButton.className = "pageBtn";
-        pageButton.onclick = () => {
-            currentPage = i;
-            renderBookList(books);
-        };
-        paginationContainer.appendChild(pageButton);
-    }
-};
-
-
-function showDetails(id) {
-    const book = books.find(b => b.id === id);
-    document.getElementById('detailTitle').innerText = book.title;
-    document.getElementById('detailPrice').innerText = `$${book.price.toFixed(2)}`;
-    document.getElementById('detailImage').src = book.link;
-    document.getElementById('detailRating').innerText = book.rating; 
-    showDetailsOn = id;
-}
-
-
-function updateBook(id) {
-    const book = books.find(b => b.id === id);
-
-    document.getElementById("bookId").value = book.id;
-    document.getElementById("bookTitle").value = book.title;
-    document.getElementById("bookPrice").value = book.price;
-    document.getElementById("bookRating").value = book.rating;
-    document.getElementById("bookImage").value = book.link;
-
-    document.getElementById("addBook").style.display = "block"; 
-
-    document.getElementById("addBookForm").dataset.mode = "update";
-    document.getElementById("addBookForm").dataset.id = id; 
-    document.getElementById("addOrUpdate").innerText = "Update"; 
-
-}
-
-function deleteBook(id) {
-    books = books.filter(book => book.id !== id);    
-    renderBookList(books);
-
-}
-
-const resetFormFields = () => {
-    document.getElementById("bookId").value = '';
-    document.getElementById("bookTitle").value = '';
-    document.getElementById("bookPrice").value = '';
-    document.getElementById("bookRating").value = '';
-    document.getElementById("bookImage").value = '';
-    document.getElementById("addOrUpdate").innerText = "Add"; 
-
-};
-
-document.getElementById("NewBook").addEventListener("click", () => {
-    resetFormFields();  
-    document.getElementById("addBook").style.display = "block"; 
-});
-
-document.getElementById("closeModal").addEventListener("click", () => {
-    document.getElementById("addBook").style.display = "none"; 
-});
-
-window.addEventListener("click", (event) => {
-    const modal = document.getElementById("addBook");
-    if (event.target === modal) {
-        modal.style.display = "none"; 
-    }
-});
-
-document.getElementById("addBookForm").addEventListener("submit", (event) => {
-    event.preventDefault(); 
-
-    const mode = event.target.dataset.mode; 
-    const bookId = event.target.dataset.id; 
-    id = parseInt(document.getElementById("bookId").value);
-    const newBook = {
-        id: id,
-        title: document.getElementById("bookTitle").value,
-        price: parseFloat(document.getElementById("bookPrice").value),
-        rating: parseInt(document.getElementById("bookRating").value),
-        link: document.getElementById("bookImage").value,
-    };
-
-    if (mode === "update") {
-        const index = books.findIndex(b => b.id === parseInt(bookId));
-        if (index !== -1) {
-            books[index] = newBook; 
-        }
-        if(id === showDetailsOn)
-            showDetails(id)
-        document.getElementById("addBookForm").dataset.mode = "new";
-    } else {
-        books.push(newBook);
-        const totalPages = Math.ceil(books.length / itemsPerPage);
-        currentPage = totalPages - 1; 
-    }
-    showDetails(id); 
-
-    document.getElementById("addBook").style.display = "none"; 
-    renderBookList(books);
-});
-
-
-
-
-
-
-
-
-
