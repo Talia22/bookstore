095c01c41f36e8241fed4c43b122ef42dbb9daf7
Talia Friedman <taliafriedman22@gmail.com>
2024-10-13 10:22:42 +0300

    finishing up last minute chnages putting in corect files and styleing

---
+++ utils.js
@@ -0,0 +1,11 @@
+function getFromLocalStorage(key) {
+    return localStorage.getItem(key);
+}
+
+function getObjFromLS(key) {
+    return JSON.parse(getFromLocalStorage(key));
+}
+
+function saveObjToLS(key, obj) {
+    localStorage.setItem(key, JSON.stringify(obj));
+}
